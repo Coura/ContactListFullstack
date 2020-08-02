b4c572f71e7c8eb80e3164ae5b7e874146f541b6
Rui Guerreiro <you@example.com>
2020-08-02 18:51:23 +0200

    about

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -4,7 +4,10 @@ const About = () => {
     return (
         <div>
             <h1>About this App</h1>
-            
+            <p>App destined to allow users to save and edit a contact listat will.</p>
+            <p>This app was developed by <a href="https://ruiguerreiro.herokuapp.com/">Rui Guerreiro</a> as an exercise for a Udemy Course "React Front To Back". </p>
+            <p>This app uses the React Context API</p>
+            <p>Version 1.0.0</p>
         </div>
     )
 }
